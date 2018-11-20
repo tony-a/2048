a91b107e28654bbad9dcf733e9d391abc8890439
Tony Askar <askar.b.tony@gmail.com>
2018-11-20 08:37:56 -0800

    adding curses library and gitignore file

--- game.py
+++ game.py
@@ -1,5 +1,10 @@
 import random 
-import copy
+import curses
+from curses import KEY_RIGHT, KEY_LEFT, KEY_DOWN, KEY_UP
+
+WIDTH = 35
+HEIGHT = 20
+TIMEOUT = 100
 
 
 class Game():
@@ -20,7 +25,8 @@ class Game():
         self._add_random_value()
         self._add_random_value()
 
-    def __repr__(self):
+    # def __repr__(self):
+    def render(self, window):
         """
         ---------------------
         |    |    |    |2048|
@@ -54,7 +60,8 @@ class Game():
 
             grid += '|\n' + '-' * line_size + '\n'
 
-        return grid
+        window.addstr(grid)
+        # return grid
 
     # def __getitem__(self, x):
     #     return self.grid[x]
@@ -161,119 +168,59 @@ class Game():
         
         return self
 
+    def swipe_down(self):
+        new_grid = {}
+        for row_index, row in enumerate(self.columns()):
+            added_row = self.add_row(row, 'r')
+            for x, value in enumerate(added_row):
+                new_grid[x, row_index] = value
+
+        if self.grid != new_grid:
+            self.grid = new_grid
+            self._add_random_value()
+
+        return self
+
 """
 reload(game)
 g = game.Game(4)
 g
-
-
 """
 
-# x = [[2,2,0,4],[2,0,4,8],[0,0,2,4], [4,2,4,0]]
-# y = [[0,2,0,0],[2048,0,4,8],[128,64,2,4], [4,2,4,0]]
+if __name__=='__main__':
+    print "hii"
+    curses.initscr()
+    curses.beep()
+    curses.beep()
+    window = curses.newwin(HEIGHT, WIDTH, 0, 0)
+    window.timeout(TIMEOUT)
+    window.keypad(1)
+    curses.noecho()
+    curses.curs_set(0)
+    window.border(0)
+
+    game = Game(4)
+
+    while True:
+        window.clear()
+        window.border(0)
+        game.render(window)
+        event = window.getch()
+
+        if event == 27:
+            break
+
+        if event == KEY_LEFT:
+            game.swipe_left()
+
+        elif event == KEY_RIGHT:
+            game.swipe_right()
+
+        elif event == KEY_UP:
+            game.swipe_up()
+
+        elif event == KEY_DOWN:
+            game.swipe_down()
 
 
-# class Grid():
-#     def __init__(self, grid_size):
-#         self.grid_size = grid_size
-#         row = [0 for x in range(0, grid_size)]
-#         self.grid = [copy.deepcopy(row) for x in range(0, grid_size)]
-#         x = random.randint(0, grid_size-1)
-#         y = random.randint(0, grid_size-1)
-#         self.grid[x][y] = 2
-
-#         x2 = random.randint(0, grid_size-1)
-#         y2 = random.randint(0, grid_size-1)
-#         self.grid[x2][y2] = 2
-
-#     def __repr__(self):
-#         """
-#         ---------------------
-#         |    |    |    |2048|
-#         ---------------------
-#         |    |    |    | 4  |
-#         ---------------------
-#         |    | 64 |    | 4  |
-#         ---------------------
-#         |    |    | 2  | 2  |
-#         ---------------------
-#         """
-#         cell_map = {0: '|    ', 
-#                     2: '| 2  ',
-#                     4: '| 4  ',
-#                     8: '| 8  ',
-#                     16: '| 16 ',
-#                     32: '| 32 ',
-#                     64: '| 64 ',
-#                     128: '| 128',
-#                     256: '| 256',
-#                     512: '| 512',
-#                     1024: '|1024',
-#                     2048: '|2048',
-#                     4096: '|4096'}
-#         line_size =  4 * self.grid_size + self.grid_size + 1
-#         grid = '-' * line_size + '\n'
-        
-#         for row in self.grid:
-#             for index, cell in enumerate(row):
-#                 grid += cell_map[cell]
-#                 if index == self.grid_size - 1:
-#                     grid += '|'
-                
-#             grid += '\n' + '-' * line_size + '\n'
-
-#         return grid
-
-#     # def __getitem__(self, x):
-#     #     return self.grid[x]
-
-#     def _strip_zero(row):
-#         new = []
-#         for item in row:
-#             if item != 0:
-#                 new.append(item)
-
-#         return new
-
-#     def add_row(self):
-#         row = self.grid[0]
-#         a = 0
-#         b = 1
-#         new = []
-#         arr = _strip_zero(row)
-#         while a < len(arr):
-#             try:
-#                 if arr[a] == arr[b]:
-#                     new.append(arr[a] + arr[b])
-#                     a += 2
-#                     b += 2
-#                 else:
-#                     new.append(arr[a])
-#                     a += 1
-#                     b += 1
-#             except IndexError:
-#                 if a < len(arr):
-#                     new.append(arr[a])
-
-#         while len(new) < len(row):
-#             new.append(0)
-
-#         return new
-
-#     def swipe_left(self):
-#         new_grid = []
-#         for row in self.grid:
-#             new_grid.append(add_row(row))
-
-#         random_added = False
-
-#         self.grid = new_grid
-
-
-
-# if __name__ == '__main__':
-#     print "hi"
-#     Grid2(4)
-
-
-
+    curses.endwin()
